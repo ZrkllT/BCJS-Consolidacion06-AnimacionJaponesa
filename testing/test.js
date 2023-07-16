9aca1d315aa9eb3ad464fa2a502ecb0924b872d7
ZrkllT <zero.kullt@gmail.com>
2023-07-16 11:49:49 -0400

    more test

--- testing/test.js
+++ testing/test.js
@@ -15,3 +15,43 @@ describe('Prueba listado Animes sin parametros (GET)', () =>{
         })
     })
 })
+
+describe('Prueba listado Animes con parametros', () =>{
+    describe('Json con ID', () =>{
+        it('Debe responder con los datos de Sailor Moon', () =>{
+            let objID = { "id":"3" }
+            chai.request(servidor).post('/listaAnimes').send(objID)
+                .end((err,res) =>{
+                    chai.expect(res)
+                    should.not.exist(err);
+                    res.body.should.be.a('objet');
+                    res.body.should.have.property('nombre')
+                    res.body.should.have.property('genero')
+                    res.body.should.have.property('año')
+                    res.body.should.have.property('autor')
+
+                    res.body.should.have.property('nombre').eql('Sailor Moon')
+                    done()
+                })
+        })
+    })
+    describe('Json con Nombre', () =>{
+        it('Debe responder con los datos de Akira', () =>{
+            let objName = { "nombre":"Akira" }
+            chai.request(servidor).post('/listaAnimes').send(objName)
+                .end((err,res) =>{
+                    chai.expect(res)
+                    should.not.exist(err);
+                    res.body.should.be.a('objet');
+                    res.body.should.have.property('nombre')
+                    res.body.should.have.property('genero')
+                    res.body.should.have.property('año')
+                    res.body.should.have.property('autor')
+
+                    res.body.should.have.property('nombre').equal('Akira')
+                    done()
+                })
+        })
+    })
+})
+
