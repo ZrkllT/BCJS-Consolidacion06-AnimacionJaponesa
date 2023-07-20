48e68562e311e1eb9aa0545bd02f60c399a76801
ZrkllT <zero.kullt@gmail.com>
2023-07-20 15:38:56 -0400

    actualizacion test

--- testing/test.js
+++ testing/test.js
@@ -15,43 +15,113 @@ describe('Prueba listado Animes sin parametros (GET)', () =>{
         })
     })
 })
-
-describe('Prueba listado Animes con parametros', () =>{
+/* listar un anime especifico */
+describe('Prueba listado Animes con parametros (POST)', () =>{
     describe('Json con ID', () =>{
-        it('Debe responder con los datos de Sailor Moon', () =>{
+        it('Debe responder con un objeto, con xx llaves', () =>{
             let objID = { "id":"3" }
-            chai.request(servidor).post('/listaAnimes').send(objID)
-                .end((err,res) =>{
+            chai.request(servidor).post('/listaAnimes').send(objID).end((err,res) =>{
                     chai.expect(res)
-                    should.not.exist(err);
+                    should.not.exist(error);
                     res.body.should.be.a('objet');
                     res.body.should.have.property('nombre')
                     res.body.should.have.property('genero')
                     res.body.should.have.property('año')
                     res.body.should.have.property('autor')
-
-                    res.body.should.have.property('nombre').eql('Sailor Moon')
                     done()
                 })
         })
     })
     describe('Json con Nombre', () =>{
-        it('Debe responder con los datos de Akira', () =>{
+        it('Debe responder con un objeto, con xx llaves', () =>{
             let objName = { "nombre":"Akira" }
-            chai.request(servidor).post('/listaAnimes').send(objName)
-                .end((err,res) =>{
+            chai.request(servidor).post('/listaAnimes').send(objName).end((err,res) =>{
                     chai.expect(res)
-                    should.not.exist(err);
+                    should.not.exist(error);
                     res.body.should.be.a('objet');
                     res.body.should.have.property('nombre')
                     res.body.should.have.property('genero')
                     res.body.should.have.property('año')
                     res.body.should.have.property('autor')
-
-                    res.body.should.have.property('nombre').equal('Akira')
                     done()
                 })
         })
     })
 })
+/* actualizar un anime */
+describe('Prueba para insertar 1 registro (POST)', () =>{
+    it('Debe responder que se ingreso correctametne (status 200)', (done) =>{
+        let objNew = {
+            "nombre": "pruebaaa3",
+            "genero": "ingreasdsaso",
+            "año": 3010,
+            "autor": "aada"
+            }
+        chai.request(servidor).post('/nuevoAnime').send(objNew).end( (err,res) =>{
+            chai.expect(res).to.have.status(200)
+            done()
+        })
+    })
+})
 
+describe('Prueba para actualizar 1 registro (PUT)', () =>{
+    describe('Actualizar Nombre', () =>{
+        it('Debe responder afirmativamente', (done)=>{
+            let objUPD = {
+                "id": "6",
+                "nombre":"solo nombre"
+            }/* el ID 6 se crea en el test anterior */
+            chai.request(servidor).put('/actualizarAnime').send(objUPD).end((err,res) =>{
+                chai.expect(res).to.have.status(200)
+                done()
+            })
+        })
+    })
+    describe('Actualizar Genero', () =>{
+        it('Debe responder afirmativamente', (done)=>{
+            let objUPD = {
+                "id": "6",
+                "genero":"solo genero"
+            }/* el ID 6 se crea en el test anterior */
+            chai.request(servidor).put('/actualizarAnime').send(objUPD).end((err,res) =>{
+                chai.expect(res).to.have.status(200)
+                done()
+            })
+        })
+    })
+    describe('Actualizar Año', () =>{
+        it('Debe responder afirmativamente', (done)=>{
+            let objUPD = {
+                "id": "6",
+                "año": 2000
+            }/* el ID 6 se crea en el test anterior */
+            chai.request(servidor).put('/actualizarAnime').send(objUPD).end((err,res) =>{
+                chai.expect(res).to.have.status(200)
+                done()
+            })
+        })
+    })
+    describe('Actualizar Autor', () =>{
+        it('Debe responder afirmativamente', (done)=>{
+            let objUPD = {
+                "id": "6",
+                "autor":"solo autor"
+            }/* el ID 6 se crea en el test anterior */
+            chai.request(servidor).put('/actualizarAnime').send(objUPD).end((err,res) =>{
+                chai.expect(res).to.have.status(200)
+                done()
+            })
+        })
+    })
+
+})
+/* eliminar un anime */
+describe('Prueba para eliminar 1 registro (DELETE)', () =>{
+    it('Debe responder afirmativamente, el Anime se crea durante el test', (done) =>{
+        let objDELETE = { "id": "6" }
+        chai.request(servidor).delete('/eliminarAnime').send(objDELETE).end((err,res) =>{
+            chai.expect(res).to.have.status(200)
+            done()
+        })
+    })
+})
